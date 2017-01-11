91116ecebec68e7d9a9b33377e41ad8b6e95022f
apple <18576469764@163.com>
2017-01-11 23:33:49 +0800

    posts

--- ReaderMovie/pages/wekcome/wekcome.js
+++ ReaderMovie/pages/wekcome/wekcome.js
@@ -1,3 +1,16 @@
 Page({
-    
+    onTap: function(event){
+        // wx.navigateTo({
+        //     url:"../posts/post"
+        // });
+        wx.redirectTo({
+            url:"../posts/post"
+        })
+    },
+    onUnload: function (){
+       // console.log("onUnload");
+    },
+    onHide: function (){
+       // console.log('onHide');
+    }
 })
